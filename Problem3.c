c3866e5f02fe441269bd896710e934974f85b485
Austin Derrow-Pinion <derrowap@rose-hulman.edu>
2016-02-21 18:41:20 -0500

    Finished Problem3 and Problem 526

--- Problem3.c
+++ Problem3.c
@@ -1,11 +1,37 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+bool prime(long long p) {
+	long long i;
+	for(i = 2; i <= (p / 2); i++) {
+		if(p % i == 0) return false;
+	}
+	return true;
+}
+
+long long largestPrime(long long num) {
+	long long i;
+	loop:
+	for(i = 2; i <= num;i++) {
+		if(prime(i)) {
+			if(i == num)
+				return i;
+			if(num % i == 0) {
+				num /= i;
+				goto loop;
+			}
+
+		}
+	}
+	return -1;
+
+}
 
 int main() {
-	long n, number = 600851475143;
-	int p[100] = {2, 3};
-	for (n = 2; n < 100; n++) {
-		if( (6*n + 1) 
-	}
-
+	// TODO: each time a number is found starting at 2 going up that divides this number, divide it. Less iterations, more efficient.
+	long long number = 600851475143;
+	// long long number = 42;
+	long long largest = largestPrime(number);
+	printf("%lld\n", largest);
 	return 0;
 }
---
+++ Problem526.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool prime(long long p);
+long long largestPrime(long long num);
+long long sumOfLargestPrimes(long long num);
+
+
+/**
+*
+**/
+bool prime(long long p) {
+	long long i;
+	for(i = 2; i <= (p / 2); i++) {
+		if(p % i == 0) return false;
+	}
+	return true;
+}
+
+/**
+* 	Returns the largest prime factor of num.
+*
+**/
+long long largestPrime(long long num) {
+	long long i;
+	loop:
+	for(i = 2; i <= num;i++) {
+		if(prime(i)) {
+			if(i == num)
+				return i;
+			if(num % i == 0) {
+				num /= i;
+				goto loop;
+			}
+
+		}
+	}
+	return -1;
+}
+
+/**
+*	f(n) if the largest prime factor of n.
+*	This returns g(n) = f(n) + f(n+1) + f(n+2) + f(n+3) +
+*		f(n+4) + f(n+5) + f(n+6) + f(n+7) + f(n+8)
+**/
+long long sumOfLargestPrimes(long long num) {
+	int i;
+	long long sum = 0;
+	for(i = 0; i < 9; i++) {
+		sum += largestPrime(num + (long long) i);
+	}
+	return sum;
+}
+
+/**
+*
+**/
+long long maxSumLargestPrime(long long num) {
+	long long i, temp, max = 0;
+	for(i = 2; i <= num; i++) {
+		temp = sumOfLargestPrimes(i);
+		if(temp > max)
+			max = temp;
+	}
+	return max;
+}
+
+int main() {
+	long long number = 1000000000;
+	long long largest = maxSumLargestPrime(number);
+	printf("%lld\n", largest);
+	return 0;
+}

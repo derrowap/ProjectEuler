68b237a3cb510fc1832601ee9ce08f644d605368
Austin Derrow-Pinion <derrowap@rose-hulman.edu>
2016-02-24 17:50:46 -0500

    Finished
    Really easy.

--- Problem6.c
+++ Problem6.c
@@ -4,28 +4,32 @@
 * Author: Austin Derrow-Pinion
 * Purpose: Solve Problem 5 on Project Euler
 * Language: C
-* 
-* TODO: UNFINISHED
 *
-* Finds the difference between the sum of the squares of 
+* Finds the difference between the sum of the squares of
 * the first n natural numbers and the square of the sum.
 */
 
-int sumOfSquare(int n) {
-
-	return -1;
+long sumOfSquare(long n) {
+	long output = 1, i;
+	for (i = 2; i <= n; i++) {
+		output += i * i;
+	}
+	return output;
 }
 
-int squareOfSum(int n) {
-
-	return -1;
+long squareOfSum(long n) {
+	long output = 1, i;
+	for (i = 2; i <= n; i++) {
+		output += i;
+	}
+	return output * output;
 }
 
 int main() {
-	int result = squareOfSum(10) - sumOfSquare(10);
+	long result = squareOfSum(10) - sumOfSquare(10);
 	printf("The difference between the sum of the squares of the first ten natural numbers and square of the sum is %d\n", result);
 
-	result = sumOfSquare(100) - sumOfSquare(100);
+	result = squareOfSum(100) - sumOfSquare(100);
 	printf("The difference between the sum of the squares of the first one hundred natural numbers and square of the sum is %d\n", result);
 
 	return 0;

18069c20f0fd6776eeb6938c22ac2e32b3351078
Austin Derrow-Pinion <derrowap@rose-hulman.edu>
2016-04-02 20:12:41 -0400

    solved

---
+++ Problem016.py
@@ -0,0 +1,22 @@
+"""
+Author: Austin Derrow-Pinion
+Purpose: Solve Problem 16 on Project Euler
+Language: Python
+
+Calculates the sum of all the digits in the
+number 2 ^ 1000 which is a 302 digit number.
+"""
+import math
+
+def sumOfDigits(n):
+	output = 0
+	while n > 9:
+ 		output += n % 10
+ 		n /= 10
+	return output + n
+
+def main():
+	print "The sum of digits in the number 2 ** 1000:", sumOfDigits(2 ** 1000)
+
+if __name__ == '__main__':
+	main()
